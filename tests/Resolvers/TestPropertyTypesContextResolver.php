b40c7514b1835f383ca3c3864ec140227bda1806
siyuedays <350375092@qq.com>
2024-11-25 15:30:59 +0800

    php-cs-fixer

--- tests/Resolvers/TestPropertyTypesContextResolver.php
+++ tests/Resolvers/TestPropertyTypesContextResolver.php
@@ -1,21 +1,20 @@
 <?php
 
-use ReflectionProperty;
 use phpDocumentor\Reflection\Fqsen;
 use phpDocumentor\Reflection\Types\Object_;
 use Astral\Serialize\Tests\TestRequest\TypeOneDoc;
 use Astral\Serialize\Resolvers\PropertyTypesContextResolver;
 
 beforeEach(function () {
-    /** @var PropertyTypesContextResolver */
+    /** @var PropertyTypesContextResolver $this */
     $this->propertyTypesContextResolver = new PropertyTypesContextResolver();
 });
 
 it('tests resolve type from doc block', function () {
     $reflectionProperty = new ReflectionProperty(TypeOneDoc::class, 'data_doc');
     $result             = $this->propertyTypesContextResolver->resolveTypeFromDocBlock($reflectionProperty);
-    expect($result)->toBeInstanceOf(Object_::class);
-    expect($result->getFqsen())->toBeInstanceOf(Fqsen::class);
-    expect($result->getFqsen()->__toString())->toBe('Astral\Serialize\Tests\TestRequest\Both\BothTypeDoc');
-    expect($result->getFqsen()->getName())->toBe('BothTypeDoc');
+    expect($result)->toBeInstanceOf(Object_::class)
+        ->and($result->getFqsen())->toBeInstanceOf(Fqsen::class)
+        ->and($result->getFqsen()->__toString())->toBe('Astral\Serialize\Tests\TestRequest\Both\BothTypeDoc')
+        ->and($result->getFqsen()->getName())->toBe('BothTypeDoc');
 });
