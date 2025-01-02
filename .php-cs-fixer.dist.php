e5c6bfae20c0f0ae38109f5eb70d255649d17eca
siyuedays <350375092@qq.com>
2025-01-02 14:15:00 +0800

    exclude tests

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,9 +5,9 @@ use PhpCsFixer\Finder;
 
 $finder = Finder::create()
     ->in(__DIR__)
-    ->exclude('vendor') // 排除目录
-    ->name('*.php') // 匹配文件
-    ->notName('*.blade.php') // 排除文件
+    ->exclude(['vendor','tests'])
+    ->name('*.php')
+    ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
@@ -17,7 +17,7 @@ return $config
         '@PSR12' => true,
         'array_syntax' => ['syntax' => 'short'],
         'binary_operator_spaces' => ['default' => 'align_single_space'],
-        'no_unused_imports' => true, // 移除未使用的 use
+        'no_unused_imports' => true,
         'single_quote' => true,
         'concat_space' => ['spacing' => 'one'],
         'no_trailing_whitespace' => true,
